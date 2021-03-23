6fe36dfe9efd90390a43161b3324cc6d8a54f068
nurmanbetowich <81219880+nurmanbetowich@users.noreply.github.com>
2021-03-23 14:42:37 +0600

    Create main.php

---
+++ main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <title>Nurumbek Akmatov</title>
+</head>
+<body style="background-color:#ebfff2;">
+ <h1>The best student</h1>
+ <p>Full Name:Nurumbek</p>
+ <p>Age:19</p>
+ <p>City:Bishkek</p>
+ <p>Email: nurymbekakmatov@gmail.com</p>
+ <p>Id Number: T180600002</p>
+ <p>Hobbies:Football</p>
+ </body>
+</html>
