ee61f65b448b3eee3bca8c99a69022516fa27e68
nurmanbetowich <81219880+nurmanbetowich@users.noreply.github.com>
2021-03-26 10:32:44 +0600

    Update main.php

--- main.php
+++ main.php
@@ -5,11 +5,11 @@
 </head>
 <body style="background-color:#ebfff2;">
  <h1>The best student</h1>
- <p>Full Name:Nurumbek</p>
- <p>Age:19</p>
- <p>City:Bishkek</p>
- <p>Email: nurymbekakmatov@gmail.com</p>
- <p>Id Number: T180600002</p>
- <p>Hobbies:Football</p>
+<p> <img src="nur.jpg" alt="Smiley face" style="float:left;width:245px;height:242px;">Full Name:Nurumbek<br><br>
+Age:19<br><br>
+City:Bishkek<br><br>
+Email: nurymbekakmatov@gmail.com<br><br>
+Id Number: T180600002<br><br>
+Hobbies:Football</p>
  </body>
 </html>
